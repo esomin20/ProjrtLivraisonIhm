c3e3a0df0aad179d07deb28f3f91c38df6493bdf
fakriaya <fakriaya2002@gamil.com>
2024-05-11 15:17:53 +0100

    yay

---
+++ src/main/java/com/example/projrtlivraisonihm/URLcontroller/UrlController.java
@@ -0,0 +1,13 @@
+package com.example.projrtlivraisonihm.URLcontroller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UrlController {
+
+    @GetMapping("/index")
+    public String showIndexPage() {
+        return "index";
+    }
+}
