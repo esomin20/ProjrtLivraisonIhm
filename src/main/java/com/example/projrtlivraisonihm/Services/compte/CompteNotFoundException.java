2ba5d1f307211775e0b00820d6e03de14736d500
Ksiham19 <133126590+Ksiham19@users.noreply.github.com>
2024-05-12 15:55:54 +0100

    Delete src/main/java/com/example/projrtlivraisonihm/Services directory

--- src/main/java/com/example/projrtlivraisonihm/Services/compte/CompteNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.projrtlivraisonihm.Services.compte;
-
-public class CompteNotFoundException extends Throwable{
-    public CompteNotFoundException(String cause) {
-        super(cause);
-    }
-}
--- src/main/java/com/example/projrtlivraisonihm/Services/test.java
+++
@@ -1,4 +0,0 @@
-package com.example.projrtlivraisonihm.Services;
-
-public class test {
-}
